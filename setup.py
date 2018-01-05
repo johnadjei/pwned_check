708ff54b74496d67a5547b4d7ade6e7c50e84483
John Adjei <john@johnadjei.com>
2018-01-04 22:10:53 -0500

    Flesh out some more package settings

--- setup.py
+++ setup.py
@@ -1,10 +1,41 @@
+# -*- coding: utf-8 -*-
+import sys
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
-setup(name='pwned_check', version='0.1',
+requires = ['click==6.7', 'pytest']
+tests_require = ['pytest', 'pytest-cache', 'pytest-cov']
+
+
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
+
+
+setup(name='pwned_check',
+      version='0.1',
       description='pwned_check package',
       url='http://github.com/truc/pwned_check',
       author='Pwned Check',
       author_email='test@example.com',
       license='MIT',
       packages=['pwned_check'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=requires,
+      entry_points={'console_scripts': ['pwned_check = pwned_check.cli:main']},
+      classifiers=[
+          'License :: OSI Approved :: BSD License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 3',
+      ],
+      extras_require={'test': tests_require},
+      cmdclass={'test': PyTest},
+      )
